3945bce847b0f9e5e5b575ff5e342f1f0091e7f2
Rakesh <rakeshkumargavara1@gmail.com>
2023-04-26 08:35:19 -0400

    additional functionalities

--- migrations/2_SimpleStorage.js
+++
@@ -1,6 +0,0 @@
-//for deploying the smart contract we use this migration code
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage);
-};
