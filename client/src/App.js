1e5e60339002b53b0afd6a191110ca71aeef2687
Rakesh <rakeshkumargavara1@gmail.com>
2023-04-26 08:30:54 -0400

    updated all funtionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import Web3 from "web3";
 import {useState, useEffect} from "react";
-//import SimpleStorage from "./contracts/SimpleStorage.json"
 import ResearchPaperBid from "./contracts/ResearchPapers.json"
 import './App.css';
+import { utils } from 'ethers';
 
 function App() {
-    var details="";
+    //var details="";
     const [state, setState] = useState({web3: null, contract: null});
     //data to show to the user
     const [data, setData] = useState("Nil");
@@ -26,13 +26,13 @@ function App() {
         //to interact with the smart contract we need two things ABI & contract address
 
         const contract = new web3.eth.Contract(ResearchPaperBid.abi, deployedNetwork.address, {
-          from: '0x2B8abeD96CA07b19c107Ab28ee5bf28F3a0e5E33',
+          from: '0xEba1B25c50e0347CA532745078AA06efF6De5b51',
           gas: '3000000'
         });
-        console.log(contract)
+        console.log(contract);
 
         //setting the state of the application
-        setState({web3: web3, contract: contract})
+        setState({web3: web3, contract: contract});
 
       }
       provider && template();
@@ -69,9 +69,7 @@ function App() {
     const author_data = document.querySelector("#value1").value;
     const paper_data = document.querySelector("#value2").value;
     //when changing data we need to tell from which account you are changing the data
-    //await contract.methods.setter(10).send({from: "0x20Ca32Bca072ECE39155516Cd22bcFDE4814E0c6"});
-    await contract.methods.uploadPaperAndValidate(author_data, paper_data).send({from: "0x2B8abeD96CA07b19c107Ab28ee5bf28F3a0e5E33"});
-    //await contract.methods.setter(data).send({from: "0x2B8abeD96CA07b19c107Ab28ee5bf28F3a0e5E33"});
+    await contract.methods.uploadPaperAndValidate(author_data, paper_data).send({from: "0xEba1B25c50e0347CA532745078AA06efF6De5b51"});
     //for refreshing the page
     window.location.reload();
   }
@@ -80,25 +78,22 @@ function App() {
   async function research_paper_details(){
     const {contract} = state;
     const tokenId = document.querySelector("#value3").value;
-    document.getElementById("address").innerHTML=21;
-    details = await contract.methods.ownerOf(tokenId);
-    console.log(contract.methods.ownerOf(tokenId))
-    //for refreshing the page
-    //window.location.reload();
-    //await iAmAPromise.then(result => result.data);
-    //set_author_address(details)
-    console.log("Printing author address for the research paper",await contract.methods.ownerOf(tokenId));
-    console.log(details);
-    return details;
+    console.log("Token id entered is, ", tokenId);
+    //document.getElementById("address").innerHTML=21;
+    //contract.methods.ownerOf(tokenId).call({from: "0xE8bf0504795BbB9e38113b38aEa9a2C40272103f"});
+    (contract.methods.ownerOf(tokenId).call({from: "0xEba1B25c50e0347CA532745078AA06efF6De5b51"})).then(data => set_author_address(data));
   }
 
   //*****************************************bidding operation****************************************
   async function open_bidding(){
     const {contract} = state;
     const tokenId = document.querySelector("#value4").value;
-    const details = await contract.methods.start_bidding(tokenId).send({from: "0x2B8abeD96CA07b19c107Ab28ee5bf28F3a0e5E33"});
+    //do we need to use call or send
+    //const details = await contract.methods.start_bidding(tokenId).call({from: "0x2D29F6760062F540F168a1fC247b44ffE533c094"});
+    
+    const details = await contract.methods.start_bidding(tokenId).send({from: "0xEba1B25c50e0347CA532745078AA06efF6De5b51"});
     //for refreshing the page
-    //window.location.reload();
+    window.location.reload();
     return details;
   }
 
@@ -106,18 +101,16 @@ function App() {
   async function bid_token(){
     const {contract} = state;
     const bidder_addr = document.querySelector("#value5").value;
+    console.log("Bidder address is ", bidder_addr);
     const bid_value = document.querySelector("#value6").value;
-    const tokenId = document.querySelector("#value4").value;
-    const details = await contract.methods.start_bidding(tokenId).send({from: bidder_addr, value: bid_value});
-    //for refreshing the page
-    //window.location.reload();
-    //return details;
+    console.log("Bid value is", (Web3.utils.toWei(bid_value.toString(), "ether")).toString());
+    await contract.methods.bid().send({from: bidder_addr, value: Web3.utils.toWei(bid_value.toString(), "ether")});
   }
 
   //close bidding
   async function close_bid(){
     const {contract} = state;
-    await contract.methods.close_bidding().send({from: "0x2B8abeD96CA07b19c107Ab28ee5bf28F3a0e5E33"});
+    await contract.methods.close_bidding().send({from: "0xEba1B25c50e0347CA532745078AA06efF6De5b51"});
     //for refreshing the page
     //window.location.reload();
   }
@@ -134,14 +127,16 @@ function App() {
       <label for="value2">Research paper name</label>
       <input type = "text" id = "value2" name = "value2"></input>
       <button onClick={mint_validate_Token}>Tokenize Research paper</button>
+      
 
       <p>Please enter token ID to find the owner of the research paper</p>
       <label for="value3">Token ID</label>
       <input type = "text" id = "value3" name = "value3"></input>
-      {/* <button onClick={research_paper_details}>Get Author address</button> */}
-      <button  onClick={() => set_author_address(research_paper_details)}>Get Author address</button>
-      <input id ="address" value = {details.author_address}/>
-      {/* <button onClick="return research_paper_details()">Get Author address</button> */}
+      <button onClick={research_paper_details}>Get Author address</button>
+      {/* <button  onClick={() => set_author_address(research_paper_details)}>Get Author address</button> */}
+      {/*printing the owner address with the id*/}
+      <p>Author address is: {author_address}</p>
+      {/* <p id="owner_address"></p> */}
 
       <p>Bidding Operations</p>
       <label for="value4">Bidding token ID</label>
@@ -153,7 +148,8 @@ function App() {
       <input type = "text" id = "value5" name = "value5"></input>
       <label for="value6">Bid Value</label>
       <input type = "text" id = "value6" name = "value6"></input>
-      <button onClick={bid_token}>Tokenize Research paper</button>
+      <button onClick={bid_token}>Bid</button>
+      
 
       <button onClick={close_bid}>Close Bidding</button>
 
